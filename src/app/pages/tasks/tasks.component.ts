0ce3f85a49d3fbbd27ef015a533a5e24fa8dc02b
Gabriel Couto <gabriel.gregorio@devmagic.com.br>
2026-01-11 20:35:22 -0300

    Step 16 - Final Component Refact (100% reactive)

--- src/app/pages/tasks/tasks.component.ts
+++ src/app/pages/tasks/tasks.component.ts
@@ -6,62 +6,95 @@ import { TaskService } from '../../core/services/task.service';
 import { Task } from '../../shared/models/task.model';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
+// @Component({
+//   selector: 'app-tasks',
+//   standalone: true,
+//   imports: [CommonModule, ReactiveFormsModule],
+//   templateUrl: './tasks.component.html',
+//   changeDetection: ChangeDetectionStrategy.OnPush
+// })
+// export class TasksComponent {
+//   // ! = “Vai ser inicializado depois, confia”
+//   // !	Definite Assignment
+//   // ?	Pode ser undefined
+//   // =	Inicialização imediata
+//   tasks$!: Observable<Task[]>;
+
+//   // SEARCH
+//   readonly searchControl = new FormControl('', { nonNullable: true });
+
+//   // ADD TASK FORM
+//   readonly addTaskForm = new FormGroup({
+//     title: new FormControl('', {
+//       nonNullable: true,
+//       validators: [Validators.required, Validators.minLength(3)]
+//     })
+//   });
+
+//   constructor(private taskService: TaskService) {
+
+//       // Fonte única de verdade (estado)
+//    this.tasks$ = this.taskService.tasks$;
+
+//     // Search reativo
+//     this.searchControl.valueChanges
+//       .pipe(
+//         debounceTime(300),
+//         distinctUntilChanged(),
+//         takeUntilDestroyed()
+//       )
+//       .subscribe(term => {
+//         this.taskService.search(term);
+//       });
+//   }
+
+//   addTask() {
+//     if (this.addTaskForm.invalid) return;
+
+//     const title = this.addTaskForm.controls.title.value;
+//     this.taskService.addTask(title);
+//     this.addTaskForm.reset();
+//   }
+
+//   toggle(task: Task) {
+//     this.taskService.toggleTask(task.id);
+//   }
+
+//   remove(task: Task) {
+//     this.taskService.removeTask(task.id);
+//   }
+
+//   trackById(_: number, task: Task) {
+//     return task.id;
+//   }
+// }
+
+
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule],
   templateUrl: './tasks.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
+
 export class TasksComponent {
-  // ! = “Vai ser inicializado depois, confia”
-  // !	Definite Assignment
-  // ?	Pode ser undefined
-  // =	Inicialização imediata
   tasks$!: Observable<Task[]>;
 
-  // SEARCH
-  readonly searchControl = new FormControl('', { nonNullable: true });
-
-  // ADD TASK FORM
-  readonly addTaskForm = new FormGroup({
-    title: new FormControl('', {
-      nonNullable: true,
-      validators: [Validators.required, Validators.minLength(3)]
-    })
-  });
-
   constructor(private taskService: TaskService) {
-
-      // Fonte única de verdade (estado)
-   this.tasks$ = this.taskService.tasks$;
-
-    // Search reativo
-    this.searchControl.valueChanges
-      .pipe(
-        debounceTime(300),
-        distinctUntilChanged(),
-        takeUntilDestroyed()
-      )
-      .subscribe(term => {
-        this.taskService.search(term);
-      });
+    this.tasks$ = this.taskService.tasks$;
   }
 
-  addTask() {
-    if (this.addTaskForm.invalid) return;
-
-    const title = this.addTaskForm.controls.title.value;
+  add(title: string) {
     this.taskService.addTask(title);
-    this.addTaskForm.reset();
   }
 
-  toggle(task: Task) {
-    this.taskService.toggleTask(task.id);
+  toggle(id: number) {
+    this.taskService.toggleTask(id);
   }
 
-  remove(task: Task) {
-    this.taskService.removeTask(task.id);
+  remove(id: number) {
+    this.taskService.removeTask(id);
   }
 
   trackById(_: number, task: Task) {
