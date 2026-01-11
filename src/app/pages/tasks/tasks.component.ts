af4753547771fa34c9ee11557a13745e7766d3bc
Gabriel Couto <gabriel.gregorio@devmagic.com.br>
2026-01-11 19:49:10 -0300

    Step 13 - Refact: Reactive Form + Validation on RXJS

--- src/app/pages/tasks/tasks.component.ts
+++ src/app/pages/tasks/tasks.component.ts
@@ -1,18 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, Observable } from 'rxjs';
 import { TaskService } from '../../core/services/task.service';
-import { debounceTime, distinctUntilChanged, Observable, switchMap } from 'rxjs';
 import { Task } from '../../shared/models/task.model';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './tasks.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-
 export class TasksComponent {
   // ! = “Vai ser inicializado depois, confia”
   // !	Definite Assignment
@@ -20,47 +20,40 @@ export class TasksComponent {
   // =	Inicialização imediata
   tasks$!: Observable<Task[]>;
 
-  searchControl = new FormControl('');
+  // SEARCH
+  readonly searchControl = new FormControl('', { nonNullable: true });
 
-
-  taskForm = new FormGroup({
-    title: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-    ]),
+  // ADD TASK FORM
+  readonly addTaskForm = new FormGroup({
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(3)]
+    })
   });
 
-  editingTaskId: number | null = null;
-  editedTitle = '';
-
-
   constructor(private taskService: TaskService) {
-    this.tasks$ = this.taskService.tasks$;
+
+      // Fonte única de verdade (estado)
+   this.tasks$ = this.taskService.tasks$;
+
+    // Search reativo
+    this.searchControl.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        takeUntilDestroyed()
+      )
+      .subscribe(term => {
+        this.taskService.search(term);
+      });
   }
 
+  addTask() {
+    if (this.addTaskForm.invalid) return;
 
-  ngOnInit() {
-    this.searchControl.valueChanges.pipe(
-      debounceTime(300),
-      distinctUntilChanged()
-    ).subscribe(term =>
-      this.taskService.search(term ?? '')
-    );
-  }
-
-  // addTask() {
-  //   if (this.taskForm.invalid) {
-  //     return;
-  //   }
-
-  //   const title = this.taskForm.value.title!;
-  //   this.taskService.addTask(title);
-  //   this.taskForm.reset();
-  // }
-
-  addTask(title: string) {
-    if (!title.trim()) return;
+    const title = this.addTaskForm.controls.title.value;
     this.taskService.addTask(title);
+    this.addTaskForm.reset();
   }
 
   toggle(task: Task) {
@@ -73,25 +66,5 @@ export class TasksComponent {
 
   trackById(_: number, task: Task) {
     return task.id;
-  }
-
-  startEdit(task: Task) {
-    this.editingTaskId = task.id;
-    this.editedTitle = task.title;
-  }
-
-
-  saveEdit(taskId: number) {
-    if (this.editedTitle.trim().length < 3) {
-      return;
-    }
-
-    this.taskService.updateTask(taskId, this.editedTitle);
-    this.cancelEdit();
-  }
-
-  cancelEdit() {
-    this.editingTaskId = null;
-    this.editedTitle = '';
   }
 }
